6f198978b1b8dfb40d2cd3cfe8e7f5ae3addff35
Carter Griffin <cartersg11@hotmail.com>
2025-03-23 14:12:59 -0400

    adjusted character physics + prototyped spring
    the player's mass and jump speed change depending on which character they're using
    
    the spring still needs some work because it gets stuck on the bottom sometimes, and it's still too bouncy for Berry

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,12 +6,16 @@ public class Player : MonoBehaviour
     public GameObject spawn;
     public GameObject berry;
     public GameObject carrot;
-    public float movementSpeed;
-    public float jumpSpeed;
     public int lives;
+    public float movementSpeed;
+    public float berryMass;
+    public float carrotMass;
+    public float berryJumpSpeed;
+    public float carrotJumpSpeed;
 
     private Rigidbody rb;
     private bool isGrounded;
+    private float jumpSpeed;
     private bool isBerry = false;
     private bool isCarrot = true;
 
@@ -33,13 +37,6 @@ public class Player : MonoBehaviour
             transform.rotation = Quaternion.LookRotation(new Vector3((Input.GetAxis("Horizontal")) * movementSpeed, 0f, 0f));
         }
 
-        // handles player jumping
-        if (Input.GetButtonDown("Jump") && isGrounded)
-        {
-            rb.AddForce(transform.up * jumpSpeed, ForceMode.Impulse);
-            isGrounded = false;
-        }
-
         // allows the player to switch between characters
         if (Input.GetKeyDown(KeyCode.LeftShift))
         {
@@ -47,6 +44,25 @@ public class Player : MonoBehaviour
             isCarrot = !isCarrot;
             berry.SetActive(isBerry);
             carrot.SetActive(isCarrot);
+        }
+
+        // adjusts the player's variables based on the character
+        if (isBerry)
+        {
+            rb.mass = berryMass;
+            jumpSpeed = berryJumpSpeed;
+        }
+        else if (isCarrot)
+        {
+            rb.mass = carrotMass;
+            jumpSpeed = carrotJumpSpeed;
+        }
+
+        // handles player jumping
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            rb.AddForce(transform.up * jumpSpeed, ForceMode.Impulse);
+            isGrounded = false;
         }
 
         // Respawns the player if they fall below the level
@@ -62,10 +78,21 @@ public class Player : MonoBehaviour
         // Checks if the player is colliding with the ground
         if (collision.gameObject.tag == "Ground")
         {
-            if (collision.contacts[0].normal.y == 1) // Checks if colliding with the top of the collision
+            // Checks if colliding with the top of the collision
+            if (collision.contacts[0].normal.y == 1)
             {
                 isGrounded = true;
             }
+        }
+    }
+
+    // Called when the player leaves a collision
+    void OnCollisionExit(Collision collision)
+    {
+        // Prevents the player from jumping in the air after falling off of a platform
+        if (collision.gameObject.tag == "Ground")
+        {
+            isGrounded = false;
         }
     }
 
